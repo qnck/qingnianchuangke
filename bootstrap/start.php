7bcc5887f3d94bd5528e9652027314cd4c7773e7
Houdi <boatkyd@aliyun.com>
2015-08-26 05:25:01 +0800

    updated -- add new PC

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('server', 'Macty'),
+	'local' => array('server', 'Macty', 'Houdi'),
 
 ));
 
