de76afc6b46ea91b092e418a2e95afc29336092b
Kydz Leo <boatkyd@gmail.com>
2015-09-11 15:35:03 +0800

    updated -- alipay callback added

--- app/controllers/PayController.php
+++ app/controllers/PayController.php
@@ -6,7 +6,7 @@ class PayController extends \BaseController
 {
     public function callbackAlipay()
     {
-        $order_id = Input::get('out_trade_no', 0);
+        $order_no = Input::get('out_trade_no', 0);
         $ali_trade_no = Input::get('trade_no', '');
         $ali_trade_status = Input::get('trade_status', '');
         $amount = Input::get('total_fee', 0);
@@ -16,14 +16,14 @@ class PayController extends \BaseController
             $alipay = new Alipay();
             $alipay->verifyNotify();
 
-            $order = Order::find($order_id);
+            $order = Order::where('o_number', '=', $order_no)->first();
             if (empty($order)) {
                 throw new Exception("没有找到订单", 9002);
             }
 
             if ($ali_trade_status == 'TRADE_FINISHED' || $ali_trade_status == 'TRADE_SUCCESS') {
                 $order->pay($amount, Alipay::PAYMENT_TAG);
-                $carts = Cart::where('o_id', '=', $order_id)->where('c_status', '=', 2)->get();
+                $carts = Cart::where('o_id', '=', $order->o_id)->where('c_status', '=', 2)->get();
                 foreach ($carts as $key => $cart) {
                     $cart->checkout();
                 }
