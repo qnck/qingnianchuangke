efa09f582ec86cb66164417678cec55ed9a86bad
Kydz Leo <boatkyd@gmail.com>
2015-09-18 02:46:17 +0800

    updated -- payment binds added

---
+++ app/models/UsersWechatPayment.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+*/
+class UsersWechatPayment extends Eloquent
+{
+    public $primaryKey = 'u_id';
+    public $timestamps = false;
+
+    public function showInList()
+    {
+        $data = [];
+        $data['account'] = $this->t_account;
+        return $data;
+    }
+}
