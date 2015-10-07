8929b30839948328a72cc6f201197609947556f9
Kydz Leo <boatkyd@gmail.com>
2015-10-07 16:18:44 +0800

    updated -- ad info return title

--- app/controllers/AdController.php
+++ app/controllers/AdController.php
@@ -7,9 +7,9 @@ class AdController extends \BaseController
     public function listIndexTop()
     {
         $data = [
-            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png'],
-            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png'],
-            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png']
+            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png', 'title' => 'title1'],
+            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png', 'title' => 'title2'],
+            ['url' => 'www.baidu.com', 'img' => 'http://www.baidu.com/img/bdlogo.png', 'title' => 'title3']
         ];
         $re = Tools::reTrue('获取首页广告成功', $data);
         return Response::json($re);
