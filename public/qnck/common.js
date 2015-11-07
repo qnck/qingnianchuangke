a1e8ee765cbb726167908406e3ba08d133ec9d15
milord <51138184@qq.com>
2015-11-08 04:07:36 +0800

    w

--- public/qnck/common.js
+++ public/qnck/common.js
@@ -1,4 +1,5 @@
-var _url = "http://www.test.54qnck.com/";
+//var _url = "http://www.test.54qnck.com/";
+var _url = "http://www.qnck.dev/";
 function GetQueryString(name)
 {
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
