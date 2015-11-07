a5c26cd3147c2cdd608c5a84cdae18c10e8740e6
milord <51138184@qq.com>
2015-11-08 03:19:08 +0800

    u

---
+++ public/qnck/common.js
@@ -0,0 +1,7 @@
+var _url = "http://www.test.54qnck.dev/";
+function GetQueryString(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
