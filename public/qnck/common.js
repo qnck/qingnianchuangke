9ce99aa7dbd6fc0232cc710865ae0f0468a1224e
milord <51138184@qq.com>
2015-11-28 10:20:20 +0800

    s

--- public/qnck/common.js
+++ public/qnck/common.js
@@ -1,6 +1,6 @@
-var _url = "http://www.test.54qnck.com/";
+//var _url = "http://www.test.54qnck.com/";
 //var _url = "http://www.54qnck.com/";
-//var _url = "http://www.qnck.dev/";
+var _url = "http://www.qnck.dev/";
 function GetQueryString(name)
 {
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
