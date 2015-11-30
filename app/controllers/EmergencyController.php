c566590d32a6b143a416d0f6486488e0b332a6e3
Kydz Liu <boatkyd@gmail.com>
2015-12-01 07:37:56 +0800

    upated

--- app/controllers/EmergencyController.php
+++ app/controllers/EmergencyController.php
@@ -72,7 +72,7 @@ class EmergencyController extends \BaseController
         echo "batch number is ".$batch."</br>";
         set_time_limit(0);
         $file = new Filesystem();
-        $re = $file->files('/tmp/qnck_fackuser');
+        $re = $file->files('/tmp/qnck_fackuser/head_img');
         foreach ($re as $key => $path) {
             $user = new User;
             $user->u_mobile = $mobile++;
