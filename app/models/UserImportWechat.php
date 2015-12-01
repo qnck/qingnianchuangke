32a6ce0706841492f1512245d7ee57d406e48c0b
Kydz Liu <boatkyd@gmail.com>
2015-12-01 15:26:49 +0800

    updated

--- app/models/UserImportWechat.php
+++ app/models/UserImportWechat.php
@@ -10,10 +10,10 @@ class UserImportWechat extends UserImport
     public function import()
     {
         $this->baseValidate();
-        $chk = UserImportWechat::where('u_ext_id', '=', $this->ext_id)->first();
+        $chk = UserImportWechat::where('u_ext_id', '=', $this->u_ext_id)->first();
         $u_id = 0;
         if (empty($chk)) {
-            $re = $this->register($this->ext_id, $this->ext_token, $this->u_nickname, $this->u_head_img, $this->u_gender);
+            $re = $this->register($this->u_ext_id, $this->u_ext_token, $this->u_nickname, $this->u_head_img, $this->u_gender);
             $this->u_id = $re['u_id'];
             $this->created_at = Tools::getNow();
             $this->save();
